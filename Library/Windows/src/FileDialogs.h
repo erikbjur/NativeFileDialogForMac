419487ffd07049928f1eb2c6ada1fab033f07138
Erik Bjur <erik@bjurengineering.com>
2024-09-25 10:44:53 -0700

    Restructured everything and addeded the windows version of the library and test app

---
+++ Library/Windows/src/FileDialogs.h
@@ -0,0 +1,10 @@
+#ifndef FILE_DIALOGS_H
+#define FILE_DIALOGS_H
+
+#include <string>
+
+// Function declarations for file dialogs
+std::string openFileDialog();
+std::string saveFileDialog();
+
+#endif // FILE_DIALOGS_H
